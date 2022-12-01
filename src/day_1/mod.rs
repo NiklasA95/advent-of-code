1b45df962eecc91450d1bd486913e6874b71fe8c
NiklasA95 <niklas.amann95@googlemail.com>
2022-12-01 14:06:10 +0100

    Fix part 2

--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -32,32 +32,44 @@ pub fn part_2() -> Result<()> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let split = contents.split("\n\n");
-    let mut three_max_calories: [i32; 3] = [0, 0, 0];
+    let blocks = contents.split("\n\n");
+    let mut portions = Vec::new();
 
-    for portion in split {
+    for portion in blocks {
         let mut calories: i32 = 0;
 
         for item in portion.lines() {
             calories += item.parse::<i32>().unwrap();
         }
 
-        for max_calories in three_max_calories.iter_mut() {
-            if calories > *max_calories {
-                *max_calories = calories;
+        portions.push(calories);
+    }
+
+    for i in 1..portions.len() {
+        let cur = portions[i];
+        let mut j = i - 1;
+
+        while portions[j] > cur {
+            portions[j + 1] = portions[j];
+            if j == 0 {
                 break;
             }
+            j -= 1;
+        }
+
+        // we exit the loop from that break statement
+        if j == 0 && portions[0] > cur {
+            portions[0] = cur;
+        } else {
+            // `portions[j] > cur` is not satsified, exit from condition judgement
+            portions[j + 1] = cur;
         }
     }
 
-    let mut three_max_calories_total: i32 = 0;
+    let sum_top_three_portions =
+        portions[portions.len() - 1] + portions[portions.len() - 2] + portions[portions.len() - 3];
 
-    for max_calories in three_max_calories {
-        println!("Max Calories: {}", max_calories);
-        three_max_calories_total += max_calories;
-    }
-
-    println!("Solution Part 2: {}", three_max_calories_total);
+    println!("Solution Part 2: {}", sum_top_three_portions);
 
     Ok(())
 }
